a5c35f4eb1e5848a07391f6c727164ce5b4d7159
David Wandar <david.wandar@gmail.com>
2012-10-20 21:21:33 +0200

    initial

---
+++ Models/Media.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vodda.Models
+{
+    public class Supplier
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Url { get; set; }
+    }
+
+    public class Media
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CoverUrl { get; set; }
+    }
+
+    public class SupplierMedia
+    {
+        [Key]
+        public int Id { get; set; }
+        public Supplier Supplier { get; set; }
+        public string SupplierIdentifier { get; set; }
+        public string Name { get; set; }
+    }
+}
